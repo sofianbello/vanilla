667934bc6721392dfb8ce8076202064a12390682
ADM-Bab1753 <ADM-Bab1753@UN-LAP-013742.ad.uni-hamburg.de>
2022-05-08 18:31:29 +0200

    added comment to debug

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -31,6 +31,7 @@ export default class Experience
         this.canvas = canvas
 
         // Setup
+        this.debug = new Debug()
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
@@ -38,7 +39,6 @@ export default class Experience
         this.camera = new Camera()
         this.renderer = new Renderer()
         this.world = new World()
-        this.debug = new Debug()
 
 
 
--- src/Experience/Utils/Debug.js
+++ src/Experience/Utils/Debug.js
@@ -5,8 +5,7 @@ export default class Debug
 {
     constructor()
     {
-        console.log('debug loading...');
-        this.active = window.location.hash === '#debug'
+        this.active = window.location.hash === '#debug' // Enables Debug only when putting '#debug' in the URL e.g. localhost:8080/#debug
         if(this.active)
         {
             this.ui = new dat.GUI()
